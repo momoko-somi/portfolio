2a752c947b587ce51b95190f7a39c46576dc7ae1
momoko-somi <70096463+momoko-somi@users.noreply.github.com>
2020-08-24 11:55:45 +0900

    Create my.script.js

---
+++ js/my.script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+   //ハンバーガーメニュー
+  document.getElementById("menuButton").addEventListener("click", function(){
+    this.classList.toggle("active");
+    document.getElementById("spHeader-menu").classList.toggle("active");
+    document.getElementById("mask").classList.toggle("active");
+    document.getElementById("title").classList.toggle("active");
+    document.getElementById("spHeader").classList.toggle("active");
+  })
+
+});
