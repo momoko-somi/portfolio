24e30c9fefe6c1671656fed36bd7d7b4a4bff789
momoko-somi <70096463+momoko-somi@users.noreply.github.com>
2020-08-26 16:02:10 +0900

    Update my.script.js

--- js/my.script.js
+++ js/my.script.js
@@ -1,9 +1,3 @@
-var delaycss = document.createElement('link');
-delaycss.rel = 'stylesheet';
-delaycss.href = '../css/style.css';
-document.head.appendChild(delaycss);
-
-
 document.addEventListener('DOMContentLoaded', function(){
    //ハンバーガーメニュー
   document.getElementById("menuButton").addEventListener("click", function(){
